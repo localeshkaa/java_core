fab0d51cfd11d888a210db0b385dab0957e8e76a
localeshkaa <loca.leshka@gmail.com>
2024-04-26 17:33:23 +0300

    core

---
+++ src/BaseSyntx/Types.java
@@ -0,0 +1,14 @@
+package BaseSyntx;
+ /*
+ доделать
+  */
+public class Types {
+    public static void main(String[] args) {
+        char charTest = charExpression();
+    }
+
+
+    public static char charExpression(int a) {
+        return '?' - 1;
+    }
+}
